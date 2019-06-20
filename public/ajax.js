9e945265d1158a3d3c363dd544068cc0d17f68eb
anydv1 <sonu.kumar@appzlogic.com>
2019-06-20 16:55:51 +0530

    added post route to create user

--- public/ajax.js
+++ public/ajax.js
@@ -1,7 +1,23 @@
-
 	
 $(document).ready(function() {
    $("#driver").click(function(event){
-      $('#stage').load('/users');
+    var data=  $('#stage').serialize();
+    $.ajax({
+      type:'Post',
+      url:'/users' ,
+      data:data,
+      success:function(user){
+         console.log(user);
+      },error:function(err){
+         console.log(err)
+      }
+    })
    });
 });
+
+	
+// $(document).ready(function() {
+//    $("#driver").click(function(event){
+//       $('#stage').load('/users');
+//    });
+// });
